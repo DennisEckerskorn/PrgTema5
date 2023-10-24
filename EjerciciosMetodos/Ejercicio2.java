055e70a36d346f5aaf873ad8750a0acc1b7494e3
DennisEckerskorn <45369024+DennisEckerskorn@users.noreply.github.com>
2023-10-24 17:04:28 +0200

    new

---
+++ EjerciciosMetodos/Ejercicio2.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Ejercicio2{
+	public static void main(String[] args){
+		Scanner lector = new Scanner(System.in);
+		double radioUsuario;
+	}
+//AQUI VAN LOS METODOS
+
+}
