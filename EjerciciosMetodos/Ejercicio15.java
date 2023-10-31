b0fb8c3c2200a22843e99986b80688c6e55e1220
Dennis Eckerskorn <45369024+DennisEckerskorn@users.noreply.github.com>
2023-11-01 00:16:20 +0100

    update15

--- EjerciciosMetodos/Ejercicio15.java
+++ EjerciciosMetodos/Ejercicio15.java
@@ -2,29 +2,51 @@ import java.util.Scanner;
 
 public class Ejercicio15{
 	public static void main(String[] args){
+		//Variables:
 		Scanner lector = new Scanner(System.in);
 		String word1, word2, word3;
 		int option;
-		boolean valid;
-		
+		boolean valid = false;
+		//Loop do-while to allow the selection of options.
 		do{
+			//We call the method showMenu which simply prints the text of the menu. Afterwards we read the value of option.
 			showMenu();
 			option = Integer.parseInt(lector.nextLine());
-			
-
-			
-			valid = option <= 0;
-		}while(!valid);
-		
-		System.out.println("Dime 3 palabras, apreta intro despues de cada una:");
-		word1 = lector.nextLine();
-		word2 = lector.nextLine();
-		word3 = lector.nextLine();
-		
-		System.out.println("La palabra más larga es: " + shortWord(word1, word2, word3));
-			
+			//Switch to execute functions when option has the value given by user. 1 - longWord, 2 - shortWord, 3 - Vocal,Accent,Upper,Lower.
+			switch(option){
+				case 0:
+					valid = true;
+				break;		
+				case 1:
+					System.out.println("Dime 3 palabras, apreta intro despues de cada una:");
+					word1 = lector.nextLine();
+					word2 = lector.nextLine();
+					word3 = lector.nextLine();
+					System.out.println("La palabra más larga es: " + longWord(word1, word2, word3));
+				break;
+				case 2:
+					System.out.println("Dime 3 palabras, apreta intro despues de cada una:");
+					word1 = lector.nextLine();
+					word2 = lector.nextLine();
+					word3 = lector.nextLine();
+					System.out.println("La palabra más corta es: " + shortWord(word1, word2, word3));
+				break;
+				case 3:
+					System.out.println("ENCUENTRA LAS VOCALES, ACENTOS, MAYÚSCULAS, MINÚSCULAS");
+					System.out.println("Dime una palabra o una frase:");
+					word1 = lector.nextLine();
+					System.out.println("La palabra tiene en total " + vocalCount(word1) + " vocales.");
+					System.out.println("La palabra tiene en total " + accentCount(word1) + " vocales con acento.");
+					System.out.println("La palabra tiene en total " + upperCaseCount(word1) + " letras en mayúscula.");
+					System.out.println("La palabra tiene en total " + lowerCaseCount(word1) + " letras en minúscula.");
+				break;
+				default:
+					System.out.println("ERROR, introduce un número entre 0 y 3");
+			}
+		}while(!valid);	
 	}
 
+	//Method to print a menu on the console screen.
 	public static void showMenu(){
 		System.out.println("MENÚ PRINCIPAL");
 		System.out.println("==============");
@@ -41,7 +63,7 @@ public class Ejercicio15{
 			return w1;
 		} else if(w2.length() >= w1.length() && w2.length() >= w3.length()){
 			return w2;
-		}else{
+		} else{
 			return w3;
 		}  
 	}
@@ -52,14 +74,58 @@ public class Ejercicio15{
 			return w1;
 		} else if(w2.length() <= w1.length() && w2.length() <= w3.length()){
 			return w2;
-		}else{
+		} else{
 			return w3;
 		}
-	
 	}
-	
-	public static String vocals(String w1){
-		int counterVocals = 0;
-		int counter
+
+	//Function to count and return how many Vocals are in the given String.
+	public static int vocalCount(String w1){
+		int vocalCounter = 0;
+		for (int i = 0; i < w1.length(); i++){
+			char ch = w1.charAt(i);
+			if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U'){
+				vocalCounter++;
+			}
+			
+		}
+		return vocalCounter;
 	}
+
+	//Function to count and return how many Vocals with accents are in the given String.
+	public static int accentCount(String w1){
+		int accentCounter = 0;
+		for (int i = 0; i < w1.length(); i++){
+			char ch = w1.charAt(i);
+			if(ch == 'á' || ch == 'é' || ch == 'í' || ch == 'ó' || ch == 'ú' || ch == 'Á' || ch == 'É' || ch == 'Í' || ch == 'Ó' || ch == 'Ú'){
+				accentCounter++;
+			}
+		}
+		return accentCounter;
+	}
+
+	//Function to count and return how many Upper Case letters are in the given String.
+	public static int upperCaseCount(String w1){
+		int counterUpperCase = 0;
+		for (int i = 0; i < w1.length(); i++){
+			char ch = w1.charAt(i);
+			if(Character.isUpperCase(ch)){
+				counterUpperCase++;
+			}
+		}
+		return counterUpperCase;	
+	}
+
+	//Function to count and return how many Lower Case letters are in the given String.
+	public static int lowerCaseCount(String w1){
+		int counterLowerCase = 0;
+		
+		for (int i = 0; i < w1.length(); i++){
+			char ch = w1.charAt(i);
+			if(Character.isLowerCase(ch)){
+				counterLowerCase++;
+			}
+		}
+		return counterLowerCase;
+	} 	
 }
