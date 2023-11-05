30107d222d9ea32933c98496f2f60c017fb2fc51
Dennis Eckerskorn <45369024+DennisEckerskorn@users.noreply.github.com>
2023-11-05 21:50:14 +0100

    Update + New 18

--- EjerciciosMetodos/Ejercicio18.java
+++ EjerciciosMetodos/Ejercicio18.java
@@ -1,5 +1,36 @@
 public class Ejercicio18 {
+    public static final int MIN_DNI = 0;
+    public static final int MAX_DNI = 99999999;
     public static void main(String[] args) {
-                
+        int dni;
+
+        dni = LibDos.solicitarDNI("Introduzca su DNI:", MIN_DNI, MAX_DNI);
+        System.out.println(LibDos.calcularNIF(dni));
     }
+    //***ESTOS MÉTODOS ESTÁN EN LibDos.java Y SE LLAMAN DESDE AHI***
+    /*Función para solicitar un entero, especificamente para un DNI.
+	public static int solicitarDNI(String mensaje, int valorMin, int valorMax){
+		boolean valido;
+		int resultado;
+		do{
+			System.out.println(mensaje);
+			resultado = Integer.parseInt(lector.nextLine());
+			valido = (resultado >= valorMin && resultado <= valorMax);
+			if (!valido){
+				System.out.printf("Error => Valor mínimo: %d, Valor máximo: %d\n", valorMin, valorMax);
+			}
+		}while (!valido);
+		lector.close();
+		return resultado;
+	}
+	Función para devolver el DNI con su caracter correspondiente.
+	public static String calcularNIF(int dni){
+        String resultado = "";
+        String caracteresNIF = "TRWAGMYFPDXBNJZSQVHLCKE";
+        int modulo = dni % 23;
+        char letra = caracteresNIF.charAt(modulo);
+        resultado += String.format("%d-%c", dni, letra);
+        return resultado;
+    }
+     */
 }
--- EjerciciosMetodos/LibDos.java
+++ EjerciciosMetodos/LibDos.java
@@ -66,4 +66,28 @@ public class LibDos {
 		resultado = lector.nextLine().charAt(0);
 		return resultado;
 	}
+	//Función para solicitar un entero, especificamente para un DNI.
+	public static int solicitarDNI(String mensaje, int valorMin, int valorMax){
+		boolean valido;
+		int resultado;
+		do{
+			System.out.println(mensaje);
+			resultado = Integer.parseInt(lector.nextLine());
+			valido = (resultado >= valorMin && resultado <= valorMax);
+			if (!valido){
+				System.out.printf("Error => Valor mínimo: %d, Valor máximo: %d\n", valorMin, valorMax);
+			}
+		}while (!valido);
+		lector.close();
+		return resultado;
+	}
+	//Función para devolver el DNI con su caracter correspondiente.
+	public static String calcularNIF(int dni){
+        String resultado = "";
+        String caracteresNIF = "TRWAGMYFPDXBNJZSQVHLCKE";
+        int modulo = dni % 23;
+        char letra = caracteresNIF.charAt(modulo);
+        resultado += String.format("%d-%c", dni, letra);
+        return resultado;
+    }
 }
